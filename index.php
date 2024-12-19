d76ed459a772cc0b7503e781bb4f8be1b42c2cb6
Donny Ardiwidha <donnyardiwidha@gmail.com>
2024-12-19 17:35:22 +0800

    menambahkan paragraf

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum voluptatem soluta odio. Dolores quidem quos autem nesciunt. Natus facere, molestias omnis sunt numquam assumenda minima autem doloribus. Corporis, explicabo labore.</p>
 </body>
 
 </html>
